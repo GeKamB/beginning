6a5ced404d66e41763a175d6c191caf5ebdc97e7
GeKamB <k.gebski1@gmail.com>
2024-01-31 19:43:43 +0000

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String myName = "Kamil";
+        System.out.println(myName);
+        System.out.println("Hello, " + myName);
+        int sum = 0;
+        sum += 1;
+        System.out.println(sum);
+        
+    }
+}
